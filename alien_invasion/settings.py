918480d1d79b0f090ecdf860ca7143ceb92e0679
saif <saif@gmail.com>
2025-05-28 10:12:11 +0000

    all comit on 28 may

---
+++ alien_invasion/settings.py
@@ -0,0 +1,28 @@
+class Settings:
+	def __init__(self):
+		self.screen_width=1200
+		self.screen_height=800
+		self.bg_color=(230,230,230)
+		self.ship_speed=5.0
+		self.bullet_speed= 1000.0
+		self.bullet_width=100000
+		self.bullet_height=3
+		self.bullet_color=(60,60,60)
+		self.bullets_allowed = 3
+		self.ship_limit = 3
+		self.alien_speed=8.0
+		self.speedup_scale=1.1
+		self.score_scale=1.5
+		self.fleet_drop_speed=10
+		self.initialize_dynamic_settings()
+	def initialize_dynamic_settings(self):
+		self.ship_speed=5.5
+		self.bullet_speed=3.0
+		self.alien_speed=5.0
+		self.fleet_direction=1
+		self.alien_points=50
+	def increase_speed(self):
+		self.ship_speed*=self.speedup_scale
+		self.bullet_speed*=self.speedup_scale
+		self.alien_speed*=self.speedup_scale
+		self.alien_points=int(self.alien_points * self.score_scale)
